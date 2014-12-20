267c83592c8187aa8454fc0af4edbb060b5d2a46
Thomas Fetcke <Thomas.Fetcke@googlemail.com>
2014-12-20 23:55:37 +0100

    Completed the script for tidy data as required for step 4 of the assignment.

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # Course Project Analysis Script
 
-# I. Data download and unpacking.
+# 1. Data download and unpacking.
 #
 # Obtain the data if not present and unpack it.
 # The ZIP file download is stored in the "download" sub-directory, the 
@@ -18,3 +18,68 @@ if (!file.exists(zipFile))
     download.file(srcUrl, zipFile, method="curl", quiet=T)
 if (!file.exists(uciDir))
     unzip(zipFile)   
+
+
+# 2. Read the source data.
+#
+# Read the column and data label files and the actual data sets.
+# Filenames as given by the source data README file.
+featuresFile <- paste(uciDir, "features.txt", sep="/")
+activitiesFile <- paste(uciDir, "activity_labels.txt", sep="/")
+trainActivitiesFile <- paste(uciDir, "train", "y_train.txt", sep="/")
+trainDataFile <- paste(uciDir, "train", "X_train.txt", sep="/")
+trainSubjectsFile <- paste(uciDir, "train", "subject_train.txt", sep="/")
+testActivitiesFile <- paste(uciDir, "test", "y_test.txt", sep="/")
+testDataFile <- paste(uciDir, "test", "X_test.txt", sep="/")
+testSubjectsFile <- paste(uciDir, "test", "subject_test.txt", sep="/")
+
+features <- read.table(featuresFile,
+                       col.names=c("ID", "Name"),
+                       colClasses=c("integer", "factor"))
+activities <- read.table(activitiesFile,
+                         col.names=c("ID", "Name"),
+                         colClasses=c("integer", "factor"))
+trainActivities <- read.table(trainActivitiesFile,
+                              col.names="ID",
+                              colClasses="integer")
+trainData <- read.table(trainDataFile, colClasses="numeric")
+trainSubjects <- read.table(trainSubjectsFile, 
+                            col.names="SubjectID",
+                            colClasses="integer")
+testActivities <- read.table(testActivitiesFile,
+                             col.names="ID",
+                             colClasses="integer")
+testData <- read.table(testDataFile, colClasses="numeric")
+testSubjects <- read.table(testSubjectsFile, 
+                           col.names="SubjectID",
+                           colClasses="integer")
+
+
+# 3. Produce a tidy data set.
+
+# 3.1 Combine the train and test data sets.
+allActivities <- rbind(trainActivities, testActivities)
+allData <- rbind(trainData, testData)
+allSubjects <- rbind(trainSubjects, testSubjects)
+
+# 3.2 Extract mean and std (deviation) columns by matching feature names.
+cols <- grep("(mean|std)\\(\\)", features$Name)
+allData <- allData[,cols]
+
+# 3.3 Apply column names.
+# Note that the columns have already been reduced to those with mean
+# or std. Thus, only those names have to be used.
+colnames(allData) <- features$Name[cols]
+
+# 3.4 Add labels to activity codes.
+# Apply the labels given in activitiy_labels.txt to the activity columns.
+allActivities$Name <- factor(allActivities$ID, 
+                             levels=activities$ID, 
+                             labels=activities$Name)
+
+# 3.5 Create the tidy data set.
+# Combine the subjects, activities and the filtered data set to
+# form the tidy data set.
+data <- cbind(Subject=allSubjects$SubjectID, 
+              Activity=allActivities$Name, 
+              allData)
